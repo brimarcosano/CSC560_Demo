786acb4b763158cff2181665a4f1c68ede09f24b
Brianna <marcosanob1@southernct.edu>
2021-10-10 19:05:25 -0400

    minor changes

--- tests.py
+++ tests.py
@@ -2,8 +2,8 @@ import os
 
 list1 = [1,2,3,4,5]
 list1.reverse()
-print(list1)
+print("My reversed list is ", list1)
 
 list2 = [10,6,8,7,9]
 list2.sort()
-print(list2)
+print("My sorted list is ", list2)
